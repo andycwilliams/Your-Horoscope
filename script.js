bc014e42c3d7c6686fdd7393ded427d7ae1a7b84
Andy <awilliamscomposing@gmail.com>
2022-01-31 22:38:40 -0800

    Adding more HTML, starting JS, applying template CSS

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const horoscopeBeginning = [];
-const horoscopeMiddle = [];
-const horoscopeEnd = [];
+const displayHoroscale = document.querySelector("#horoscope-container");
+
+const horoscopeBeginning = ["You will find a mysterious stranger", "You will wake up"];
+const horoscopeMiddle = ["and your favorite food", "and suddenly your entire family"];
+const horoscopeEnd = ["will swallow you whole.", "will recount in great detail your most embarrassing memory from 3rd grade."];
 
 const generateHoroscope = document.querySelector("#generate-horoscope");
