cf9fa84a22ae8a563f442fe5bed33c904cefc7d5
Andy <awilliamscomposing@gmail.com>
2022-02-04 02:41:02 -0800

    Another simple fix for sign not displaying

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
 const displayHoroscope = document.querySelector("#horoscope-container");
 const generateButton = document.querySelector("#generate-horoscope");
-const getSign = document.querySelector("#signs").value;
-// const strangeAlbert = document.querySelector("#strangeAlbert");
-const signsArray = [
-  "Aries",
-  "Taurus",
-  "Gemini",
-  "Cancer",
-  "Leo",
-  "Virgo",
-  "Libra",
-  "Scorpio",
-  "Sagittarius",
-  "Capricorn",
-  "Aquarius",
-  "Pisces",
-];
-
-console.log(getSign);
 
 const horoscopeSubject = [
   "Your third favorite cousin",
@@ -31,24 +13,25 @@ const horoscopeSubject = [
 ];
 
 const horoscopeBeginning = [
-  "will approach you",
+  "will approach you with an offer: make a vision board and",
   "will wake up",
   "will find a most enchanting orb",
-  "will run into",
-  "will stumble across",
+  "will run into you",
+  "will stumble across an aviary",
 ];
 const horoscopeMiddle = [
   "your favorite food",
-  "with tidings both fair and foul: The Grand Cosmic Space Cube speaks to you",
+  "with tidings both fair and foul: The Grand Cosmic Space Cube shall speak to you",
   "and get into a two-day long fistfight",
+  "and the concept of spacetime itself",
 ];
 const horoscopeEnd = [
   "ERROR_405: We're sorry, the 7th moon is in retrograde. Please check your fortune again later. We apologize for any inconvenience or summoning portals opened.",
   "before swallowing you whole.",
-  "with the 2005 principal cast of Billy Elliot.",
+  "before finally getting roasted by the entire 2005 principal cast of Billy Elliot.",
   "with just whatever is under the sink.",
   "then recount in excruciating detail your most embarrassing memory from 3rd grade.",
-  "challenge you to an arm wrestling contest. You will lose dozens of times in a row. Then, finally, one time you will feel like it's going to be different. You feel different. Stronger, more capable. Your opponent seems fatigued. Are they giving up? You press the advantage. After half an hour it seems like they're going to yield. Sweat erupts from their every pore. You're also wearing out but gosh darn are you DETERMINED. Then suddenly they SLAM your arm to the other side, spraining it in three places. Everyone is laughing. Your parents are laughing the hardest.",
+  "and then challenge you to an arm wrestling contest. You will lose dozens of times in a row. Then, finally, one time you will feel like it's going to be different. You feel different. Stronger, more capable. Your opponent seems fatigued. Are they giving up? You press the advantage. After half an hour it seems like they're going to yield. Sweat erupts from their every pore. You're also wearing out but gosh darn are you DETERMINED. Then suddenly they SLAM your arm to the other side, spraining it in three places. Everyone is laughing. Your parents are laughing the hardest.",
 ];
 
 function spinMyWebOfFate() {
@@ -57,6 +40,7 @@ function spinMyWebOfFate() {
   const randomMid = Math.floor(Math.random() * horoscopeMiddle.length);
   const randomEnd = Math.floor(Math.random() * horoscopeEnd.length);
   const strangeAlbert = document.querySelector("#strangeAlbert");
+  const getSign = document.querySelector("#signs").value;
 
   const newHoroscope = `${getSign.toUpperCase()}: ${
     horoscopeSubject[randomSub]
@@ -70,10 +54,8 @@ function spinMyWebOfFate() {
   displayHoroscope.innerHTML = newHoroscope;
   // console.log(newHoroscope);
 
-  if (strangeAlbert.style.display === "block") {
+  if (strangeAlbert.style.display === "none") {
     strangeAlbert.style.display = "block";
-  } else {
-    strangeAlbert.style.display = "none";
   }
 }
 
