976766d440e040d5529d7c3c6b18ac3c2167381a
Andy <awilliamscomposing@gmail.com>
2024-01-13 18:32:04 -0800

    Fixing and abstracting logic

--- scripts/script.js
+++ scripts/script.js
@@ -37,27 +37,87 @@ const horoscopeEnd = [
 ];
 const horoscopeStinger = ["Ba'hee Priss Dimmie!"];
 
-const getZodiacSign = () => {
-  const userSign = document.querySelector("#sign-selector").value;
-  // const displayHoroscope2 = document.querySelector(
-  //   "#generate-horoscope-button"
-  // ).value;
- const selectedDate = document.querySelector('#birthday-selector');
+// Function to determine where the input came from
 
+const determineInputSource = () => {
+  const signInput = document.querySelector("#sign-selector").value;
+  const dateInput = document.querySelector("#birth-date-selector").valueAsDate;
 
-  console.log(userSign);
-  // console.log(displayHoroscope2);
-
-  spinMyWebOfFate();
+  if (dateInput && signInput !== "default") {
+    console.log("Both Sign and Birth Date selected");
+    result = getSignFromBirthDate(dateInput.toString());
+  } else if (dateInput) {
+    console.log("Only Birth Date selected");
+    result = getSignFromBirthDate(dateInput.toString());
+  } else if (signInput !== "default") {
+    console.log("Only Sign selected");
+    result = spinMyWebOfFate(signInput);
+  } else {
+    console.log("NEITHER, OR ERROR");
+    result = null;
+  }
 };
 
-const spinMyWebOfFate = () => {
+// Function to derive Zodiac sign from the date selector
+
+const getSignFromBirthDate = (dateInput) => {
+  const dateObject = new Date(dateInput);
+  const month = dateObject.toLocaleString("en-US", { month: "long" });
+  const day = dateObject.getDate();
+
+  let userSign = "";
+
+  if (month == "January") {
+    if (day < 20) userSign = "Capricorn";
+    else userSign = "Aquarius";
+  } else if (month == "February") {
+    if (day < 19) userSign = "Aquarius";
+    else userSign = "Pisces";
+  } else if (month == "March") {
+    if (day < 21) userSign = "Pisces";
+    else userSign = "Aries";
+  } else if (month == "April") {
+    if (day < 20) userSign = "Aries";
+    else userSign = "Taurus";
+  } else if (month == "May") {
+    if (day < 21) userSign = "Taurus";
+    else userSign = "Gemini";
+  } else if (month == "June") {
+    if (day < 21) userSign = "Gemini";
+    else userSign = "Cancer";
+  } else if (month == "July") {
+    if (day < 23) userSign = "Cancer";
+    else userSign = "Leo";
+  } else if (month == "August") {
+    if (day < 23) userSign = "Leo";
+    else userSign = "Virgo";
+  } else if (month == "September") {
+    if (day < 23) userSign = "Virgo";
+    else userSign = "Libra";
+  } else if (month == "October") {
+    if (day < 23) userSign = "Libra";
+    else userSign = "Scorpio";
+  } else if (month == "November") {
+    if (day < 22) userSign = "Scorpio";
+    else userSign = "Sagittarius";
+  } else if (month == "December") {
+    if (day < 22) userSign = "Sagittarius";
+    else userSign = "Capricorn";
+  } else {
+    console.log("Something else happened, I guess");
+  }
+
+  spinMyWebOfFate(userSign);
+};
+
+// Function to call upon the heavens and deliver their message
+
+const spinMyWebOfFate = (getSign) => {
   const randomSub = Math.floor(Math.random() * horoscopeSubject.length);
   const randomBeg = Math.floor(Math.random() * horoscopeBeginning.length);
   const randomMid = Math.floor(Math.random() * horoscopeMiddle.length);
   const randomEnd = Math.floor(Math.random() * horoscopeEnd.length);
   const strangeAlbert = document.querySelector("#peculiar-albert");
-  const getSign = document.querySelector("#sign-selector").value;
 
   const newHoroscope = `${getSign.toUpperCase()}: ${
     horoscopeSubject[randomSub]
